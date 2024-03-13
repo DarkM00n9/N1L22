ef8d1f7961c587aba5ac679935d87990df1688f5
DarkMoon <noamcodm@gmail.com>
2024-03-13 23:57:45 +0100

    Initial Commit

---
+++ N1L2/N1L2App.swift
@@ -0,0 +1,17 @@
+//
+//  N1L2App.swift
+//  N1L2
+//
+//  Created by noamm on 13/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct N1L2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
